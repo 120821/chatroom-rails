64f6a8d1ff55a9cc67b83fd55ca4a9dfb2a8e373
申思维 <shensiwei@sina.com>
2022-08-16 10:15:52 +0800

    修改配置，在生产模式可以运行

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module ChatroomRails
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Beijing"
+    config.middleware.delete Rack::Lock
 # config.active_record.default_timezone = :Beijing
   end
 end
